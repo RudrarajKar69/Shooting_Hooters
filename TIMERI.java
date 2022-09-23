17e661654b173a2eb9d1f99f426b3f173486cb17
RudrarajKar69 <108626884+RudrarajKar69@users.noreply.github.com>
2022-09-23 23:50:51 +0530

    Add files via upload

---
+++ TIMERI.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+
+public class TIMERI implements ActionListener
+{
+	Timer t;
+	public static int TIME;
+	public static boolean j;
+	TIMERI(){
+		TIME=1;
+		j=false;
+		t=new Timer(1000,this);
+		t.start();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		TIME--;
+		if(TIME<=0) {
+			t.stop();
+			j=true;
+		}
+	}
+	
+}
